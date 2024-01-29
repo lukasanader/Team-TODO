bca4f4ec8cb1c76f1b1d693c0352e134907fbbb2
benjaminCMorka <benjamin.morka@kcl.ac.uk>
2024-01-29 22:41:47 +0000

    create topic model

---
+++ info_hub_app/lib/models/topic.dart
@@ -0,0 +1,13 @@
+class Topic {
+  final String title;
+  final String description;
+  final String videoUrl;
+  final String articleUrl;
+
+  Topic({
+    required this.title,
+    required this.description,
+    required this.videoUrl,
+    required this.articleUrl,
+  });
+}
