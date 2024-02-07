e0311124d1ea8a0a144c9913ec83a0f13b065a58
Bruno Miguel Fonseca Cavaca (k22017608) <bruno.fonseca_cavaca@kcl.ac.uk>
2024-02-07 12:43:15 +0000

    Complete HomePage Tests
    - All tests have been complete for the frontend for this screen, two tests need fixing and additional implementation before they can be performed. After these tests are complete, 100% coverage will be attained.

---
+++ info_hub_app/test/home_page_test.dart
@@ -0,0 +1,46 @@
+import 'package:info_hub_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:info_hub_app/screens/home_page.dart'; // Import your home page file
+import 'package:info_hub_app/screens/registration_screen.dart';
+
+void main() {
+  
+  // setUpAll(() async {
+  //   WidgetsFlutterBinding.ensureInitialized();
+  //   await Firebase.initializeApp(
+  //     options: DefaultFirebaseOptions.currentPlatform,
+  //   );
+  // });
+  
+  testWidgets('Register button is present', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: HomePage()));
+    expect(find.text('Register'), findsOneWidget);
+  });
+
+  testWidgets('Login button is present', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: HomePage()));
+    expect(find.text('Login'), findsOneWidget);
+  });
+
+  testWidgets('Image is present', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: HomePage()));
+    expect(find.image(const AssetImage('assets/base_image.png')), findsOneWidget); 
+  });
+
+  // testWidgets('Register button press leads to register screen', (WidgetTester tester) async {
+  //   await tester.pumpWidget(MaterialApp(home: HomePage()));
+  //   await tester.tap(find.text('Register'));
+  //   await tester.pumpAndSettle();
+  //   expect(find.byType(RegistrationScreen), findsOneWidget);
+  // });
+
+  // testWidgets('Login button press leads to login screen', (WidgetTester tester) async {
+  //   await tester.pumpWidget(MaterialApp(home: HomePage()));
+  //   await tester.tap(find.text('Login'));
+  //   await tester.pumpAndSettle();
+  //   expect(find.byType(LoginScreen), findsOneWidget);
+  // });
+
+}
