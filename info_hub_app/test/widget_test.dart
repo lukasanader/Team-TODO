f053363496a2817a05381a52b8ecb17c7b25071b
Luka Sanader <lukasanader@hotmail.com>
2024-02-08 13:52:19 +0000

    updated tests

--- info_hub_app/test/widget_test.dart
+++ info_hub_app/test/widget_test.dart
@@ -15,8 +15,6 @@ import 'package:info_hub_app/screens/trending_topic.dart';
 void main() {
   testWidgets('Trendings topic are in right order', (WidgetTester tester) async {
     // Build your widget
-    
-    
     final firestore = FakeFirebaseFirestore();
     CollectionReference topicCollectionRef =
         firestore.collection('topics');
@@ -60,5 +58,83 @@ void main() {
     expect((textFinders.first.evaluate().single.widget as Text).data, 'test 1');
     expect((textFinders.at(1).evaluate().single.widget as Text).data, 'test 2');
     expect((textFinders.last.evaluate().single.widget as Text).data, 'test 3');
+  });
+
+   testWidgets('Shows only first 6 trending topics', (WidgetTester tester) async {
+    // Build your widget
+    final firestore = FakeFirebaseFirestore();
+    CollectionReference topicCollectionRef =
+        firestore.collection('topics');
+    topicCollectionRef.add({
+      'title': 'test 1',
+      'description': 'this is a test',
+      'articleLink': '',
+      'videoUrl': '',
+      'views': 10,
+      'date': DateTime.now(),
+    });
+    topicCollectionRef.add({
+      'title': 'test 2',
+      'description': 'this is a test',
+      'articleLink': '',
+      'videoUrl': '',
+      'views': 9,
+      'date': DateTime.now(),
+    });
+    topicCollectionRef.add({
+      'title': 'test 3',
+      'description': 'this is a test',
+      'articleLink': '',
+      'videoUrl': '',
+      'views': 8,
+      'date': DateTime.now(),
+    });
+    topicCollectionRef.add({
+      'title': 'test 4',
+      'description': 'this is a test',
+      'articleLink': '',
+      'videoUrl': '',
+      'views': 7,
+      'date': DateTime.now(),
+    });
+    topicCollectionRef.add({
+      'title': 'test 5',
+      'description': 'this is a test',
+      'articleLink': '',
+      'videoUrl': '',
+      'views': 6,
+      'date': DateTime.now(),
+    });
+    topicCollectionRef.add({
+      'title': 'test 6',
+      'description': 'this is a test',
+      'articleLink': '',
+      'videoUrl': '',
+      'views': 5,
+      'date': DateTime.now(),
+    });
+    topicCollectionRef.add({
+      'title': 'test 7',
+      'description': 'this is a test',
+      'articleLink': '',
+      'videoUrl': '',
+      'views': 4,
+      'date': DateTime.now(),
+    });
+
+    await tester.pumpWidget(MyApp(firestore: firestore));
+    await tester.pumpAndSettle();
+
+    // Tap into the ListView
+    Finder listViewFinder = find.byType(ListView);
+    expect(listViewFinder, findsOneWidget);
+
+    // Get the list of cards
+    Finder cardFinder = find.byType(Card);
+    expect(cardFinder, findsNWidgets(6));
+
+    final textFinders = find.byType(Text);
+    // Check that test 7 is ignored
+    expect((textFinders.last.evaluate().single.widget as Text).data, 'test 6');
   });
 }
