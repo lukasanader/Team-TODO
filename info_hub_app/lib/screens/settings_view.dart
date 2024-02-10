14d811f1ecfb8d5da3fcd6059794b29c570abecd
Murbarkh <murbarkh.hamid@gmail.com>
2024-02-10 12:51:17 +0000

    Create screen for settings view. Add basic appbar for now

---
+++ info_hub_app/lib/screens/settings_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingsView extends StatelessWidget {
+  const SettingsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+        leading: IconButton(
+          onPressed: () {}, 
+          icon: const Icon(Icons.arrow_back))
+      ),
+    );
+  }
+}
