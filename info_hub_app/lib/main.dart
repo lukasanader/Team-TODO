7bcc69ce9eb42d0fdda0d023f89917323afb7d1a
Sharujan Sriramasothy <sharujan.sriramasothy@kcl.ac.uk>
2024-02-12 19:40:13 +0000

    Second attempt at home page using basepage.dart
    This is similar to the first attempt.

--- info_hub_app/lib/main.dart
+++ info_hub_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/homepage.dart';
+import 'screens/basepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
