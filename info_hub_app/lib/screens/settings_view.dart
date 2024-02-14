ce2b8d6c99542ed2409f0301b855b89eee2eac9f
Murbarkh <murbarkh.hamid@gmail.com>
2024-02-14 12:58:50 +0000

    Finish settings page. Add tests for settings page

--- info_hub_app/lib/screens/settings_view.dart
+++ info_hub_app/lib/screens/settings_view.dart
@@ -12,6 +12,62 @@ class SettingsView extends StatelessWidget {
           onPressed: () {}, 
           icon: const Icon(Icons.arrow_back))
       ),
+      body: ListView(
+        children: [
+            Container(
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.black)
+              // ),
+              child: const ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  foregroundColor: Color.fromRGBO(226, 4, 4, 0.612),
+                  backgroundImage: AssetImage('assets/blank_pfp.png'),
+                ),
+                title: Text("Username"),
+                subtitle: Text("Role"),
+              ),    
+            ),
+
+
+            Container(
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.black)
+              // ),
+              child: const ListTile(
+                leading: Icon(Icons.notifications),
+                title: Text('Manage Notifications'),
+              ),
+            ),
+            Container(
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.black)
+              // ),
+              child: const ListTile(
+                leading: Icon(Icons.privacy_tip),
+                title: Text('Manage Privacy Settings'),
+              ),
+            ),
+            Container(
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.black)
+              // ),
+              child: const ListTile(
+                leading: Icon(Icons.history_outlined),
+                title: Text('History'),
+              ),
+            ),
+            Container(
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.black)
+              // ),
+              child: const ListTile(
+                leading: Icon(Icons.help),
+                title: Text('Help'),
+              ),
+            ),
+
+        ],)
     );
   }
 }
