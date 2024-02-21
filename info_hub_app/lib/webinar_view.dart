72413df9633052a8c469ad42b63e8a5b613b5820
Murbarkh <murbarkh.hamid@gmail.com>
2024-02-21 16:54:00 +0000

    Add webinar card for future. Add basic design layout for webinar view

--- info_hub_app/lib/webinar_view.dart
+++ info_hub_app/lib/webinar_view.dart
@@ -13,8 +13,56 @@ class _WebinarViewState extends State<WebinarView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Upcoming Webinars"),
+        title: const Text("Webinars"),
       ),
+      body: 1 == 1
+      ? SafeArea(
+        child: Column(
+          children: [
+            const Text("Currently Live"),
+            Expanded(
+              flex: 1,
+              child: ListView.builder(
+                itemCount: 2,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Text(index.toString()),
+                  );
+                }
+              ),
+            ),
+            const Text("Upcoming Webinars"),
+            Expanded(
+              flex: 5,
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Text(index.toString()),
+                  );
+                }
+              ),
+            ),
+          ],
+        ),
+      )
+      : SafeArea(
+          child: Column(
+            children: [
+              const Text("Upcoming Webinars"),
+              Expanded(
+                child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Text(index.toString()),
+                  );
+                }
+              ),
+            ),
+          ],
+        ) 
+      )
     );
   }
 }
