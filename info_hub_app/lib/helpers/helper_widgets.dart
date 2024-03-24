cb244b0c96aa7ff491125db9d23b6e37c08581de
Sharujan Sriramasothy <k21046137@kcl.ac.uk>
2024-03-24 22:01:23 +0000

    Add styling for empty live, upcoming and archive webinars

--- info_hub_app/lib/helpers/helper_widgets.dart
+++ info_hub_app/lib/helpers/helper_widgets.dart
@@ -3,6 +3,7 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:info_hub_app/theme/theme_constants.dart';
 
 Widget addVerticalSpace(double height) {
   return SizedBox(
@@ -15,3 +16,25 @@ Widget addVerticalSpace(double height) {
 //     width: width,
 //   );
 // }
+
+// Used to display a message in a card
+Widget messageCard(String message, String messageKey) {
+  return Padding(
+    key: ValueKey(messageKey),
+    padding: const EdgeInsets.symmetric(horizontal: 15),
+    child: Card(
+      surfaceTintColor: COLOR_PRIMARY_LIGHT,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          message,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: COLOR_SECONDARY_GREY_LIGHT_DARKER,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    ),
+  );
+}
