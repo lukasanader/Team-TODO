811f6cf458ad9480a328093ed13956e0a9a47eae
Murbarkh <murbarkh.hamid@gmail.com>
2024-03-24 15:40:32 +0000

    Test to show messages are deleted along with message rooms

--- info_hub_app/test/message_tests/admin_message_view_test.dart
+++ info_hub_app/test/message_tests/admin_message_view_test.dart
@@ -184,6 +184,30 @@ void main() {
 
   testWidgets('Delete button deletes message room',
       (WidgetTester tester) async {
+
+    firestore
+      .collection('message_rooms')
+      .doc('1')
+      .collection('messages')
+      .add({
+        'senderId' : auth.currentUser!.uid,
+        'receiverId' : '1',
+        'message' : 'Hello world',
+        'timestamp' : DateTime.now(),
+      });
+    
+
+    //verify there is a message in the database
+    QuerySnapshot messages = await firestore
+      .collection('message_rooms')
+      .doc('1')
+      .collection('messages')
+      .get();
+
+    expect(messages.docs.isNotEmpty, true);
+
+
+
     // Build our app and trigger a frame.
     await tester.pumpWidget(adminMessageViewWidget);
     await tester.pumpAndSettle();
@@ -201,13 +225,16 @@ void main() {
     await tester.tap(find.text('OK'));
     await tester.pumpAndSettle();
 
-    QuerySnapshot messages = await firestore
+    messages = await firestore
       .collection('message_rooms')
       .doc('1')
       .collection('messages')
       .get();
 
+
+    //verify that the message is also now deleted
     expect(messages.docs.isEmpty, true);
+
 
     expect(find.text('user@gmail.com'), findsNothing);    
   });
